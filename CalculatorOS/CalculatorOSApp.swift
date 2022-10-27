c28c396b2fad8787d1c2a733d74144ea0ca94e87
Tornelius <tkbjr28@icloud.com>
2022-10-27 19:00:32 -0400

    Initial Commit

---
+++ CalculatorOS/CalculatorOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorOSApp.swift
+//  CalculatorOS
+//
+//  Created by Tornelius Broadwater, Jr on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
